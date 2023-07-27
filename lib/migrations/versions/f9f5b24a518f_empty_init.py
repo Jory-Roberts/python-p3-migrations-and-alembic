70308cbf9b943c60761375f50f8930a6c9274dad
Jory Roberts <robertsjory13@gmail.com>
2023-07-26 23:59:16 -0500

    complete

---
+++ lib/migrations/versions/f9f5b24a518f_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: f9f5b24a518f
+Revises: 
+Create Date: 2023-07-26 23:48:05.315550
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f9f5b24a518f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
